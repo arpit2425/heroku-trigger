7ed16b16767874f4eec69a552ca2be487339b543
arpit2425 <arpittrivedi2425@gmail.com>
2020-12-18 15:36:08 +0530

    json parse error

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ const sub_package_query=`query vas_sub_packages($id: uuid){
 
 
  app.post('/tracker-trigger',async(req, res) => {
-        
+    console.log(req.body);
         const adminSecret = process.env.HASURA_ADMIN_SECRET;
         const hgeEndpoint = process.env.HASURA_GQL_URL;
-        console.log(JSON.parse(req.body.payload));
+      
         try{
           const { event: {op, data}, table: {name, schema} } = JSON.parse(req.body.payload);
           let {created_by,created_at,modified_at,modified_by,deleted,properties,id,log_remarks,start_date,end_date}=data.new;
